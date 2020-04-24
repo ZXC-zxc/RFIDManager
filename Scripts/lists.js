8f5006fc37cbf2ffa25906c7c6f13f394ed8dfa5
zxckiller <xiaochen@ftsafe.com>
2020-04-24 17:52:23 +0800

    增加外部输入数据的检查

--- Scripts/lists.js
+++ Scripts/lists.js
@@ -31,7 +31,7 @@ function addBind(){
     var reader_ipport = $("#newReaderIP").val();
     var equ_ipport = $("#newEquIP").val();
     var vms_no = $("#newVMSNo").val();
-    //检查sn是否重复
+    //检查sn是否重复,如果重复走update，如果没重复走new
     var isRepeat = false;
     for(var i=0;i<EqumentList.length;i++){
         if(sn == EqumentList[i].equ_sn){
@@ -39,6 +39,25 @@ function addBind(){
             break;
         }
     }    
+    for(var i=0;i<EqumentList.length;i++){
+        if(vms_no == EqumentList[i].equ_no){
+            alert("VMS编号已定，请勿重复绑定");
+            return ;
+        }
+    }    
+
+    var reg=new RegExp(/^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\:([0-9]|[1-9]\d{1,3}|[1-5]\d{4}|6[0-5]{2}[0-3][0-5])$/);
+    if(!reg.test(reader_ipport))
+    { 
+        alert("读写器IP格式不正确!应该是127.0.0.1:8080这种格式"); 
+        return;
+    } 
+    if(!reg.test(equ_ipport))
+    { 
+        alert("屏显IP格式不正确!应该是127.0.0.1:8080这种格式"); 
+        return;
+    } 
+    
     //检查ip地址
 
     if(!isRepeat){
